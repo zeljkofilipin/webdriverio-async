a80bba941cc59b2d915a5d11f30dad9989d3b504
Željko Filipin <zeljko.filipin@gmail.com>
2021-12-14 18:16:57 +0100

    cleanup: don't use deleted page

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,5 +1,4 @@
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
