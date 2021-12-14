e823bd6920edd7255921278b91ee18f16c0e969e
Željko Filipin <zeljko.filipin@gmail.com>
2021-12-14 18:14:20 +0100

    log in to local mediawiki

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -10,15 +10,15 @@ class LoginPage extends Page {
      * define selectors using getter methods
      */
     get inputUsername() {
-        return $('#username');
+        return $('#wpName1');
     }
 
     get inputPassword() {
-        return $('#password');
+        return $('#wpPassword1');
     }
 
     get btnSubmit() {
-        return $('button[type="submit"]');
+        return $('#wpLoginAttempt');
     }
 
     /**
@@ -35,7 +35,7 @@ class LoginPage extends Page {
      * overwrite specific options to adapt it to page object
      */
     open() {
-        return super.open('login');
+        return super.open('Special:UserLogin');
     }
 }
 
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -5,10 +5,7 @@ describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        await LoginPage.login('Admin', 'dockerpass');
     });
 });
 
